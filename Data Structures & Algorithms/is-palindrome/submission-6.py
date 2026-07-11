ae59dc5d429ce032e583db585420171f633000d8
Avneet Aurora <148987776+Avi702@users.noreply.github.com>
2026-07-10 22:32:17 -0400

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        a = s.lower()
+        b = a.split(" ")
+        print(b)
+        k = "".join(b)
+        l = 0
+        r = len(k) -1
+        while l<r:
+            while l<r and k[l] not in "abcdefghijklmnopqrstuvwxyz0123456789":
+                l+=1
+            while l<r and k[r] not in "abcdefghijklmnopqrstuvwxyz0123456789":
+                r-=1
+            if k[l]==k[r]:
+                l+=1
+                r-=1
+            else:
+                return False
+
+
+        return True
