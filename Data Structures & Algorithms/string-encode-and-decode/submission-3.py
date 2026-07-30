17f4fec0238b0f376d54575f4bceef38bfcc6546
Avneet Aurora <148987776+Avi702@users.noreply.github.com>
2026-07-30 17:50:29 -0400

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        ans = ""
+        for i in strs:
+            ans += str(len(i)) + "_" + i
+        return ans
+    def decode(self, s: str) -> List[str]:
+        ans = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "_":
+                j += 1
+            length = int(s[i:j])
+            ans.append(s[j+1:j+ length+1])
+            i = j + length + 1
+        return ans
