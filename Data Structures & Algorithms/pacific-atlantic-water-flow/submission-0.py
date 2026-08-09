5882137d4faa05a0f78d44bd4b0a2446847498b6
Avneet Aurora <148987776+Avi702@users.noreply.github.com>
2026-08-09 13:27:36 -0400

    Add: pacific-atlantic-water-flow - submission-0

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-0.py
@@ -0,0 +1,37 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        m = len(heights)
+        n = len(heights[0])
+        self.Pacific = False
+        self.Atlantic = False
+        ans = []
+        def waterFlow(r,c,val,visited):
+            if r < 0 or c < 0:
+                self.Pacific = True
+                return
+            if r >= m or c >= n:
+                self.Atlantic = True
+                return
+            if (r,c) in visited:
+                return
+            if val < heights[r][c]:
+                return
+            visited.add((r,c))
+            if self.Atlantic and self.Pacific:
+                return
+            waterFlow(r+1,c,heights[r][c],visited)
+            waterFlow(r-1,c,heights[r][c],visited)
+            waterFlow(r,c-1,heights[r][c],visited)
+            waterFlow(r,c+1,heights[r][c],visited)
+        
+        for row in range(m):
+            for col in range(n):
+                self.Pacific = False
+                self.Atlantic = False
+                visited = set()
+                waterFlow(row,col,heights[row][col],visited)
+                if self.Pacific and self.Atlantic:
+                    ans.append([row,col])
+        return ans
+
+            
