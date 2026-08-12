c2faf2bcf1ff63edd3cfebe4c21769d01da92467
Avneet Aurora <148987776+Avi702@users.noreply.github.com>
2026-08-12 08:29:26 -0400

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    from collections import defaultdict
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        mydict = defaultdict(list)
+        seen = set()
+        for e, s in prerequisites:
+            mydict[s].append(e)
+        def dfs(course):
+            if course in seen:
+                return False
+            if mydict[course] == []:
+                return True
+            seen.add(course)
+            for nei in mydict[course]:
+                res = dfs(nei)
+                if not res:
+                    return False
+            seen.remove(course)
+            mydict[course] = []
+            return True
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+        return True
+        
+        
