8129f561c3b100a8dff6b229f25d02bf093986ea
Avneet Aurora <148987776+Avi702@users.noreply.github.com>
2026-08-12 16:18:48 -0400

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    from collections import Counter, deque
+    import heapq
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        time = 0
+        k = Counter(tasks)
+        task = []
+        for key, val in k.items():
+            task.append([-val,0,key])
+        heapq.heapify(task)
+        q = deque()
+        while task:
+            if q and q[0][1] == time:
+                count, nextCycle, func = q.popleft()
+                heapq.heappush(task,[count,nextCycle,func])
+            count, nextCycle, func = heapq.heappop(task)
+            if count < -1:
+                q.append([count+1,time+n+1,func])
+            time += 1
+        while q:
+            if time == q[0][1]:
+                count, nextCycle, func = q.popleft()
+                if count < -1:
+                    q.append([count+1,time+n+1,func])
+            time += 1
+        return time
