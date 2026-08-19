d7a2a5a40dfc466c0c6e0d758a4a92ec1698c777
Avneet Aurora <148987776+Avi702@users.noreply.github.com>
2026-08-19 18:19:58 -0400

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def trySpeed(s):
+            newPiles = piles
+            time = 0
+            for bananas in newPiles:
+                time += (bananas + s - 1)//s
+            return time
+        L = 1
+        R = max(piles)
+        mintime = R
+        while L <= R:
+            M = (L+R)//2
+            time = trySpeed(M)
+            if time > h:
+                L = M + 1
+            elif time <= h:
+                mintime = min(M,mintime)
+                R = M - 1
+        return mintime
+
