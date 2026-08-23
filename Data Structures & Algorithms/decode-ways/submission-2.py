c3da0440c3a287b8697e144d2ed125065cf3a983
Avneet Aurora <148987776+Avi702@users.noreply.github.com>
2026-08-23 11:41:22 -0400

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        #top-down approach
+        n = len(s)
+        self.cache = {n:1,n+1:1}
+        def dfs(i):
+            if i in self.cache:
+                return self.cache[i]
+            if s[i] == "0":
+                return 0
+            if i + 1 not in self.cache:
+                self.cache[i+1] = dfs(i+1)
+            res = self.cache[i+1]
+            if i + 1 < n and int(s[i:i+2]) < 27:
+                if i + 2 not in self.cache:
+                    self.cache[i+2] = dfs(i+2)
+                res += self.cache[i+2]
+            return res
+        return dfs(0)
+        
