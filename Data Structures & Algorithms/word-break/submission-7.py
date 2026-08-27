c843ce385e34b957b8eedef8aca86cf698515b0e
Avneet Aurora <148987776+Avi702@users.noreply.github.com>
2026-08-26 21:19:48 -0400

    Add: word-break - submission-7

---
+++ Data Structures & Algorithms/word-break/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        sset = set(wordDict)
+        n = len(s)
+        self.memo = {n:True}
+        def dfs(i):
+            if i in self.memo:
+                return self.memo[i]
+            for j in range(i,n):
+                if s[i:j+1] in sset:
+                    self.memo[i] = dfs(j+1)
+                    if self.memo[i]:
+                        return True
+            self.memo[i] = False
+            return False
+        return dfs(0)
+                
