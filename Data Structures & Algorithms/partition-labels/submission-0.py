eea9f0a674521f0b3694f94100db2ed32f8de68b
Avneet Aurora <148987776+Avi702@users.noreply.github.com>
2026-09-10 22:59:50 -0400

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        lastIndex = {}
+        n = len(s)
+        for i in range(n):
+            lastIndex[s[i]] = i
+        L = 0
+        ans = []
+        sset = set()
+        R = 0
+        last =0
+        while R < n:
+            if s[R] not in sset:
+                sset.add(s[R])
+                last = max(last,lastIndex[s[R]])
+            if last == R:
+                ans.append(R-L+1)
+                sset = set()
+                L = R + 1
+            R+=1
+        return ans
+
+            
